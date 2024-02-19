c0c583098f0cbfe95ee87e9d90880bd97b72e410
Carlos Pacheco <carlospacheco.fx@gmail.com>
2024-02-19 06:15:09 -0400

    user.model created and configurated

---
+++ server/models/user.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const UserModel = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", UserModel);
