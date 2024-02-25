6d42af65d9e57cf36e37c619578de4934e0efb50
Carlos Pacheco <carlospacheco.fx@gmail.com>
2024-02-25 16:21:27 -0400

    Routes created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,21 @@
 import { Box, Container } from "@mui/material";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Register, Login } from "./routes";
 const App = () => {
   return (
-    <Box>
-      <Container sx={{ background: "red" }}>App</Container>
-    </Box>
+    <BrowserRouter>
+      <Container>
+        <Routes>
+          <Route path="/" element={<Box>App</Box>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/products" element={<Box>Products</Box>} />
+          <Route path="/addProduct" element={<Box>Create Product</Box>} />
+          <Route path="/products/:id" element={<Box>Update Product</Box>} />
+          <Route path="/profile" element={<Box>Profile</Box>} />
+        </Routes>
+      </Container>
+    </BrowserRouter>
   );
 };
 
