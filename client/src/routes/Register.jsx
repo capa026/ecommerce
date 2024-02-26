e75b37cb9a48939ce6d5e1627780097df49ce120
Carlos Pacheco <carlospacheco.fx@gmail.com>
2024-02-25 21:00:31 -0400

    Register page modified

--- client/src/routes/Register.jsx
+++ client/src/routes/Register.jsx
@@ -11,12 +11,17 @@ import {
   Typography,
   Link as L,
 } from "@mui/material";
-import { Visibility, VisibilityOff } from "@mui/icons-material";
+import {
+  Groups2Outlined,
+  Visibility,
+  VisibilityOff,
+} from "@mui/icons-material";
 import { useForm } from "react-hook-form";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 const Register = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const { register } = useForm();
   return (
     <Stack justifyContent="center" alignItems="center" height="100vh">
@@ -31,6 +36,15 @@ const Register = () => {
         boxShadow="0 0 15px -3px black"
         width={{ xs: "80%", md: "40%" }}
       >
+        <Stack
+          fontSize="2rem"
+          flexDirection="row"
+          alignItems="center"
+          gap="0.5rem"
+        >
+          <Groups2Outlined fontSize="1rem" />
+          Register
+        </Stack>
         <TextField label="First Name" variant="filled" fullWidth />
         <TextField label="Last Name" variant="filled" fullWidth />
         <TextField label="Email" variant="filled" fullWidth />
@@ -44,6 +58,26 @@ const Register = () => {
                 <IconButton
                   aria-label="toggle password visibility"
                   onClick={() => setShowPassword(!showPassword)}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+        <FormControl variant="filled" fullWidth>
+          <InputLabel htmlFor="filled-adornment-password">
+            Confirm Password
+          </InputLabel>
+          <FilledInput
+            id="filled-adornment-password"
+            type={showConfirmPassword ? "text" : "password"}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={() => setShowConfirmPassword(!showConfirmPassword)}
                   edge="end"
                 >
                   {showPassword ? <VisibilityOff /> : <Visibility />}
